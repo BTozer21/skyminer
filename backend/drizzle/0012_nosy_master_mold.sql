9058ef0b98c9ba1717bc07788ad45510125bc02d
Benji <benji.tozer@icloud.com>
2026-08-09 16:31:39 +0100

    adding in the database changes

---
+++ backend/drizzle/0012_nosy_master_mold.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "jobs" ADD COLUMN "quote" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "jobs" ADD COLUMN "rams" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "jobs" ADD COLUMN "po" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "jobs" ADD COLUMN "report" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "jobs" ADD COLUMN "invoice" boolean DEFAULT false;
