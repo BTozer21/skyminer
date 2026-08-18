016a79aa1a8565bfc6cc48e8e687e23cce84d9ab
Benji <benji.tozer@icloud.com>
2026-08-18 20:30:04 +0100

    adding an isPending block in the customer page

--- frontend/src/routes/_authenticated.admin.customers.$customerId.tsx
+++ frontend/src/routes/_authenticated.admin.customers.$customerId.tsx
@@ -19,9 +19,11 @@ function RouteComponent() {
   return (
     <div className="flex h-full flex-col px-5 pb-5">
       <div className="mb-4 mt-2 flex shrink-0 justify-between items-center">
-        <h1 className="font-bold text-xl">
-          {customer.name}
-        </h1>
+        {!isPending &&
+          <h1 className="font-bold text-xl">
+            {customer.name}
+          </h1>
+        }
       </div>
     </div>
   )
